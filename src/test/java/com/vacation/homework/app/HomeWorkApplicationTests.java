38ddc24eb028395754c96d70726b18d10b0468ca
seohyun <seohyun@chungbuk.ac.kr>
2025-06-11 05:03:58 +0900

    feat: 실행파일 추가

---
+++ src/test/java/com/vacation/homework/app/HomeWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vacation.homework.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
